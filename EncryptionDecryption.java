4e1c8681643b0ee90eac2a7d2b0123ae66efef97
Areej AlOtaibi <72670290+AreejTurky@users.noreply.github.com>
2021-11-06 20:25:15 +0300

    Update EncryptionDecryption.java
    Delete Duplication

--- EncryptionDecryption.java
+++ EncryptionDecryption.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 public class EncryptionDecryption{
 
@@ -35,41 +34,3 @@ public class EncryptionDecryption{
 		Alice();
 	}
 }
-=======
-
-public class EncryptionDecryption{
-
-	public static void Alice()
-	{
-		String s="hello new world";
-		int key=5;
-		char[] chars=s.toCharArray();
-		 
-		
-		for(int i=0 ; i< chars.length ; i++)
-		{
-				chars[i] +=key;
-		}
-	
-		String enc= String.valueOf(chars);
-		System.out.println(enc);
-		Bob(enc, key);
-	}
-
-	public static void Bob(String word , int key)
-	{
-		char[] chars=word.toCharArray();
-
-		for(int i=0 ; i< chars.length ; i++)
-		{
-				chars[i] -=key;
-		}
-
-		String enc= String.valueOf(chars);
-		System.out.println(enc);
-	}
-	public static void main(String[] args) {
-		Alice();
-	}
-}
->>>>>>> 9db893e357d11c73adc703c7ca1819a5839efd86
